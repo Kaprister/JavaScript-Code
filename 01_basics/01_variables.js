2c7520479e5c3c89e4b2247abb4829682a5af20b
SUSHANT KUMAR KAPRI <121545565+Kaprister@users.noreply.github.com>
2024-01-11 10:34:09 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 14453
+let accountEmail = "sushant@gamil.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2;
+accountEmail = "hc@hc.com"
+accountPassword = "12245231"
+accountCity = "bangal"
+
+console.log(accountId);
+
+/*
+prefer not to use var 
+because issue in block scope
+*/
+
+
+console.log([accountId, accountEmail, accountPassword, accountCity, accountState]);
