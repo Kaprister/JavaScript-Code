74bfcf5ac3acb10ff34abc91434317476261cb85
SUSHANT KUMAR KAPRI <121545565+Kaprister@users.noreply.github.com>
2024-01-16 07:55:18 +0000

    mini hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -11,7 +11,7 @@ if(true){
     const b = 20
     // var c = 30
     // c = 70
-    console.log("INNER : ", a);
+    // console.log("INNER : ", a);
 }
 
 // for(let i = 0;i < Array.length; i++){
@@ -19,6 +19,55 @@ if(true){
 // }
 
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+function one(){
+    const username = "sushant"
+
+    function two(){
+        const website = "youtube"
+        // console.log(username);
+    }
+    // console.log(website);
+    two();
+}
+
+// one();
+
+
+if(true){
+    const username = "sushant"
+    if(username === "sushant"){
+        const website = "youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+
+
+
+// +++++++++++++++++++++++ interesting +++++++++++++++
+
+console.log(addone(5));
+
+
+function addone(num){
+    return num + 1
+}
+
+// addone(5)
+
+// console.log(addtwo(5)); // gives error because of hoisting
+
+const addtwo = function(num){
+    return num + 2
+}
+
+// addtwo(5)
