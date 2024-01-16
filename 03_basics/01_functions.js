d0059425e724412e85e1245278b6a469ed8adba6
SUSHANT KUMAR KAPRI <121545565+Kaprister@users.noreply.github.com>
2024-01-16 07:35:32 +0000

    functions and add some properties in devcontainer

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,76 @@
+function sayMyName(){
+    console.log("s");
+    console.log("u");
+    console.log("s");
+    console.log("h");
+    console.log("a");
+    console.log("n");
+    console.log("t");
+}
+
+// sayMyName()
+
+// function addTwoNumbers(number1, number2){
+
+//     console.log(number1 + number2);
+// }
+
+function addTwoNumbers(number1, number2){
+
+    // let result = number1 + number2
+    // return result
+    return number1 + number2
+}
+
+const result = addTwoNumbers(3, 5)
+
+// console.log("Result: ", result);
+
+
+function loginUserMessage(username = "sam"){
+    if(!username){
+        console.log("PLease enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+
+// console.log(loginUserMessage("sushant"))
+// console.log(loginUserMessage("sushant"))
+
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 1000));
+
+
+const user = {
+    username : "sushant",
+    price : 199
+}
+
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+
+handleObject({
+    username : "sam",
+    price : 399
+})
+
+const myNewArray = [200, 300, 400, 500]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+
+
+
+
+
